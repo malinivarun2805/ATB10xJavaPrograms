40d4be38eb6306ac41b45930ce798f504f8ecab3
Malini <malinivarun2805@gmail.com>
2025-01-07 20:37:38 +0530

    SecondCommit

---
+++ src/ex01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,14 @@
+package ex01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        System.out.println(2+3);
+        // sum of 2 numbers
+        /* Arithmetic operations
+        add
+        subtract
+        multiply
+        division
+         */
+    }
+}
